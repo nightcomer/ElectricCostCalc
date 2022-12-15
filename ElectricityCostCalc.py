79d6e3541ba4865a3615f06e83462cc689af016f
Tariq <tariqalamleh1@gmail.com>
2022-12-14 21:31:31 -0500

    added exception handling

--- ElectricityCostCalc.py
+++ ElectricityCostCalc.py
@@ -1,13 +1,17 @@
-W = input("Consumption (Watts): ")
-H = input("Use Per Day (Hours): ")
-cost = input("Cost per kWh (Dollars): ")
+try:
+    W = input("Consumption (Watts): ")
+    H = input("Use Per Day (Hours): ")
+    cost = input("Cost per kWh (Dollars): ")
 
-kwh = (float(W) * float(H)) / 1000
-daily = (float(kwh) * float(cost))
-weekly = daily * 7
-yearly = daily * 365
+    kwh = (float(W) * float(H)) / 1000
+    daily = (float(kwh) * float(cost))
+    weekly = daily * 7
+    yearly = daily * 365
 
-print("Consumption per day: " + str(kwh) + "kWh")
-print("Electricity Cost Per Day: " + "${:,.2f}".format(daily))
-print("Electricity Cost Per Week: " + "${:,.2f}".format(weekly))
-print("Electricity Cost Per Year: " + "${:,.2f}".format(yearly))
+    print("Consumption per day: " + str(kwh) + "kWh")
+    print("Electricity Cost Per Day: " + "${:,.2f}".format(daily))
+    print("Electricity Cost Per Week: " + "${:,.2f}".format(weekly))
+    print("Electricity Cost Per Year: " + "${:,.2f}".format(yearly))
+
+except ValueError or NameError:
+    print("At least one input is invalid.")
